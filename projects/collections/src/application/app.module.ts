f38a253ae97b4cfe082aec2ce1fdb30bf4ef0c89
marcospss <marcospss@gmail.com>
2021-03-25 22:25:43 -0300

    Config services

--- projects/collections/src/application/app.module.ts
+++ projects/collections/src/application/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from '../views/home/app.component';
@@ -10,6 +11,7 @@ import { AppComponent } from '../views/home/app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
