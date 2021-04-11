2ed28d49563743d9df98d8141bba1ba0b235d9e7
marcospss <marcospss@gmail.com>
2021-04-11 17:36:28 -0300

    Set Env projects

--- projects/details/src/infrastructure/environments/environment.prod.ts
+++ projects/details/src/infrastructure/environments/environment.prod.ts
@@ -1,7 +1,3 @@
 export const environment = {
   production: true,
-  API_KEY: process.env.API_KEY,
-  baseURL: 'https://api.themoviedb.org/3',
-  language: 'en-US',
-  paramsDefault: `api_key=${process.env.API_KEY}&language=en-US`
 };
